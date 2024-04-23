6dacdd389f8a6da194369b75d124b7238c2b13f0
ShererdGraham <shererd94@gmail.com>
2024-04-23 23:17:47 +0100

    Added ordering of an item

--- cafe.py
+++ cafe.py
@@ -19,14 +19,29 @@ print(
 '''Welcome to the Coding Cafe
 Here's our menu\n''')
 
-total_stock_value = 0
 
-# Loop through menu to get number and cost of each item and print each one
-for item in menu:    
-    print(f"{item}: £{price[item]}... only {stock[item]} left!")
+while(True):
     
-    # Create total stock value by summing the multiple the amount of each item and its price
-    total_stock_value += stock[item] * price[item]
+    total_stock_value = 0
+    # Loop through menu to get number and cost of each item and print each one
+    for item in menu:       
+        print(f"{item}: £{price[item]}... only {stock[item]} left!")
+        
+        # Create total stock value by summing the multiple the amount of each item and its price
+        total_stock_value += stock[item] * price[item]
 
-# Print the total value of the stock
-print(f"\nAnd if you want to buy it all, it'll be £{total_stock_value}!")
+    # Print the total value of the stock
+    print(f"\nAnd if you want to buy it all, it'll be £{total_stock_value}!")
+
+
+    item_picked = input("What item would you like? Type done when your order is finished\n")        
+    cost_of_order = 0
+    for item in menu:    
+        if item == item_picked:
+            cost_of_order += price[item]
+            stock[item] -= 1
+    if item_picked == "done":
+        break
+
+    print(f"That will be £{cost_of_order}\n")
+
